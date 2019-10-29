080c5ce75e0de4978850e5c99be6b226f90d1ff0
elchangtw <elchang.tw@gmail.com>
2019-10-28 22:31:27 -0700

    basically everything

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,11 @@
-class Bee {
+class Bee extends Grub {
   // TODO..
+  constructor() {
+    super();
+    this.age = 5;
+    this.color = 'yellow';
+    // a food property that is inherited from grub
+    // an eat method that is inherited from grub
+    this.job = 'Keep on growing';
+  }
 };
--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,16 @@
-class ForagerBee {
+class ForagerBee extends Bee {
   // TODO..
+  constructor() {
+    super();
+    this.age = 10;
+    this.job = 'find pollen';
+    // a color property inherited from bee that is set to yellow
+    // a food property that is inherited from grub
+    // an eat method that is inherited from grub
+    this.canFly = true;
+    this.treasureChest = [];
+  }
+  forage(treasure) {
+    this.treasureChest.push(treasure);
+  }
 };
